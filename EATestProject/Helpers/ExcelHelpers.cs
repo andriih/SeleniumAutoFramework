e138fbc724bc90213c785263d39fb9f0eab612df
andrii.hnatyshyn <andrii.hnatyshyn@gmail.com>
2019-03-23 22:17:50 +0200

    ExcelHelpers: added DataTable methods

--- EATestProject/Helpers/ExcelHelpers.cs
+++ EATestProject/Helpers/ExcelHelpers.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using Excel;
+using System;
 using System.Collections.Generic;
+using System.Data;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +11,51 @@ namespace EAAutoFramework.Helpers
 {
     class ExcelHelpers
     {
+        private static List<Datacollection> dataCol = new List<Datacollection>();
+
+        public static void PopulateCollection(string fileName)
+        {
+            DataTable table = ExcelToDataTable(fileName);
+
+            for (int row = 1; row <= table.Rows.Count; row++)
+            {
+                for (int col = 0; col < table.Columns.Count; col++)
+                {
+                    Datacollection dtTable = new Datacollection()
+                    {
+                        rowNumber = row,
+                        colName = table.Columns[col].ColumnName,
+                        colValue = table.Rows[row - 1][col].ToString()
+                    };
+
+                    dataCol.Add(dtTable);
+                }
+            }
+        }
+
+        private static DataTable ExcelToDataTable(string fileName)
+        {
+            FileStream stream = File.Open(fileName, FileMode.Open, FileAccess.Read);
+
+            IExcelDataReader excelReader = ExcelReaderFactory.CreateOpenXmlReader(stream);
+
+            excelReader.IsFirstRowAsColumnNames = true;
+
+            DataSet result = excelReader.AsDataSet();
+
+            DataTableCollection table = result.Tables;
+
+            DataTable resultTable = table["Sheet1"];
+
+            return resultTable;
+        }
+
+        public class Datacollection
+        {
+            public int rowNumber { get; set;}
+            public string colName { get; set; }
+            public string colValue { get; set; }
+
+        }
     }
 }
